3ac8655eb04b32b6a9a565bda40deb6bf1416f7d
JasonChang <jason.chang@tpisoftware.com>
2022-11-16 16:29:47 +0800

    商品功能 - 重構程式

--- src/main/java/com/chichibonbon/springbootmall/controller/ProductController.java
+++ src/main/java/com/chichibonbon/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.chichibonbon.springbootmall.dto.ProductQueryParams;
 import com.chichibonbon.springbootmall.dto.ProductRequest;
 import com.chichibonbon.springbootmall.model.Product;
 import com.chichibonbon.springbootmall.service.ProductService;
+import com.chichibonbon.springbootmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,7 +36,7 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             //查詢件 Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -56,9 +57,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        //取得product list
         List<Product> productList = productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        //取得product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        //分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @PostMapping("/products")
